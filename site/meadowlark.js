799bdf7876096bb29bffc37599cb011666ca23ac
yaojm <yaojm@guahao.com>
2022-01-19 20:18:43 +0800

    feature:初步框架

---
+++ site/meadowlark.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const engine = require('express-handlebars')
+const app = express()
+
+app.engine('handlebars', engine.engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/about', (req, res) => {
+  const fortunes = [
+    '姚俊敏',
+    '魏贵东',
+    '王金玉'
+  ]
+  res.render('about', {fortune: fortunes[Math.floor(Math.random() * fortunes.length)]})
+})
+
+// 定制404页
+app.use((req, res) => {
+  res.status(404)
+  res.render('404')
+})
+
+// 定制500页
+app.use((err, req, next) => {
+  console.error(err.message)
+  res.status(500)
+  res.render('500')
+})
+
+app.listen(port, () => console.log(`
+express started on http://localhost:${port};press ctrl-c to terminate.
+`))
